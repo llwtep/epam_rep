79ce08c6e3efc4d25381ab969675ad5067c6429d
Azamjon <azamahmedov2005@gmail.com>
2024-11-08 19:43:44 +0500

    add test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+
+# def fact(num):
+#     if num == 1 or num == 0:
+#         return 1;
+#     return num*fact(num-1)
+#
+# def func(n):
+#     res=0
+#     for i in range(0, n+1):
+#        res+=(n**i)/fact(i)
+#     return res
+# print(func(6))
