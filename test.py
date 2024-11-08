0c643630437887cba98c6fc10038662b86d3ff6d
llwtep <azamahmedov2005@gmail.com>
2024-11-08 19:50:10 +0500

    Update test.py add print_message functions

--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 
-# def fact(num):
-#     if num == 1 or num == 0:
-#         return 1;
-#     return num*fact(num-1)
-#
-# def func(n):
-#     res=0
-#     for i in range(0, n+1):
-#        res+=(n**i)/fact(i)
-#     return res
-# print(func(6))
+def fact(num):
+    if num == 1 or num == 0:
+        return 1;
+    return num*fact(num-1)
+
+def func(n):
+    res=0
+    for i in range(0, n+1):
+       res+=(n**i)/fact(i)
+    return res
+print(func(6))
+
+def print_message():
+  print('Hello from github')
+print_message()  
